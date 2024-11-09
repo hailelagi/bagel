72e2f75cece4bcc7d63cf450a2a2657e9d0d1c31
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-11-09 01:44:10 +0100

    wip refactor
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- bagel-cpp/main.cc
+++ bagel-cpp/main.cc
@@ -1,48 +1,4 @@
-#include <iostream>
-
-struct Node {
-    int data;
-    Node* next;
-};
-
-class LinkedList {
-public:
-    LinkedList() : head(nullptr) {}
-    ~LinkedList() {
-      Node *temp;
-      while (head) {
-        temp = head;
-        head = head->next;
-        delete temp;
-      }
-    }
-
-    void append(int data) {
-        Node* newNode = new Node{data, nullptr};
-        if (!head) {
-            head = newNode;
-        } else {
-            Node* temp = head;
-            while (temp->next) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
-
-    void print() const {
-        Node* temp = head;
-        while (temp) {
-            std::cout << temp->data << " -> ";
-            temp = temp->next;
-        }
-        std::cout << "nullptr" << std::endl;
-    }
-
-
-private:
-    Node* head;
-};
+#include "free_list.h"
 
 int main() {
     LinkedList list;
